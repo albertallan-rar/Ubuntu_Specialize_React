db78f5b9756034d5cac3368068fc5df8c29ea09f
AlbertAllan <albertallanramos@gmail.com>
2023-05-10 12:29:10 -0300

    Estilizando Pagina Css

--- Ubuntu-React/src/pages/Home/Index.tsx
+++ Ubuntu-React/src/pages/Home/Index.tsx
@@ -1,12 +1,12 @@
-import styles from './styles.css'
+import styles from './styles.module.css'
 
 function Home() {
     return (
-    <>
+    <div className={styles.container}>
         <h1>HOME</h1>
         <input type='text' placeholder="Digite alguma coisa"></input>
         <button type="button">Adicionar</button>
-    </>
+    </div>
     
     )
 }
