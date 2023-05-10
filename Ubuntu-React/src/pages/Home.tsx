4aaa33d8f3514142dd6420c8b22ff2ba86c121bf
AlbertAllan <albertallanramos@gmail.com>
2023-05-10 11:59:19 -0300

    Fragments

--- Ubuntu-React/src/pages/Home.tsx
+++ Ubuntu-React/src/pages/Home.tsx
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>HOME</h1>
+    return (
+    <>
+        <h1>HOME</h1>
+        <input type='text' placeholder="Digite alguma coisa"></input>
+        <button type="button">Adicionar</button>
+    </>
+    
+    )
 }
 
 export default Home
