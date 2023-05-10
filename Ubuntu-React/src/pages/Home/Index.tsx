275e75565673ccf69de20af5a356e75e8b98ee43
AlbertAllan <albertallanramos@gmail.com>
2023-05-10 14:48:29 -0300

    Hooks

--- Ubuntu-React/src/pages/Home/Index.tsx
+++ Ubuntu-React/src/pages/Home/Index.tsx
@@ -1,7 +1,10 @@
+//Import Hooks
 import {useState} from 'react'
 
+//Import Styles applied on the page
 import styles from './styles.module.css'
 
+//Import of external files used on this page
 import PresenceArea from '../../components/Presence_Card'
 
 interface Student{
@@ -16,10 +19,12 @@ function Home() {
 
     const handleAddStudent= () => {
 
+        //Check if the name field is empty
         if(studentName.trim() === ''){
             return;
         }
 
+        //Add new students to the list
         const newStudent = {
             name: studentName,
             time: new Date().toLocaleDateString("pt-br",{
@@ -29,6 +34,7 @@ function Home() {
             })
         };
         
+        //Adds to the list joining the existing students in it
         setStundents((prevStudents) => [...prevStudents, newStudent])
         setStudentName('')
     }
@@ -45,6 +51,7 @@ function Home() {
         <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
         {
+            //Scrolls through the list and displays on the screen 
             stundents.map((student,index) =>(<PresenceArea key={index} name={student.name} time={student.time}/>) 
            )
         
