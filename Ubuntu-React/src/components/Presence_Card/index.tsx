1212b4ca8cf3af6b25505d1289ca17e7dfccc760
AlbertAllan <albertallanramos@gmail.com>
2023-05-10 12:50:39 -0300

    Components

---
+++ Ubuntu-React/src/components/Presence_Card/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css'
+
+function PresenceArea() {
+    return (
+        <div className={styles.card}>
+            <strong>Presence Area</strong>
+            <small>Teste</small>
+        </div>
+    )
+}
+
+export default PresenceArea
--- Ubuntu-React/src/pages/Home/Index.tsx
+++ Ubuntu-React/src/pages/Home/Index.tsx
@@ -1,11 +1,17 @@
 import styles from './styles.module.css'
 
+import PresenceArea from '../../components/Presence_Card'
+
 function Home() {
     return (
     <div className={styles.container}>
         <h1>HOME</h1>
         <input type='text' placeholder="Digite alguma coisa"></input>
         <button type="button">Adicionar</button>
+
+        <PresenceArea />
+        <PresenceArea />
+        <PresenceArea />
     </div>
     
     )
